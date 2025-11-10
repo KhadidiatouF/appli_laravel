b86685c6f95e8b40640e5cd3fef79da0e694a186
KhadidiatouF <fallkhadidiatou0103@gmail.com>
2025-11-10 22:24:01 +0000

    test deploy

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -92,7 +92,7 @@ return [
             /*
              * Edit to set the api's base path
              */
-            'base' => env('L5_SWAGGER_BASE_PATH', null),
+            'base' => env('L5_SWAGGER_BASE_PATH', 'https://appli-laravel-groupe.onrender.com'),
 
             /*
              * Absolute path to directories that should be excluded from scanning
@@ -313,7 +313,7 @@ return [
          * Constants which can be used in annotations
          */
         'constants' => [
-            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://my-default-host.com'),
+            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'https://appli-laravel-groupe.onrender.com'),
         ],
     ],
 ];
